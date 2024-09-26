2a3eb1bc517598635a53dc8bfa8604a613fa151d
David Gonzalez <jdgonzalez8137@soy.sena.edu.co>
2024-09-26 15:42:59 -0500

    Agregue un numero archivo llamado felicitaciones, en el cual diriguira al usuario cuando hallaacertado el numero secreto, tambien hice una modificacion en el javascript y la pagina inicial

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 let numeroSecreto = generarNumeroSecreto();
 let intentos = 0;
 
-
 function asignarTextoElemento(elemento, texto) {
     let elementoHTML = document.querySelector(elemento);
     elementoHTML.innerHTML = texto;
@@ -12,14 +11,13 @@ function verificarIntento() {
     let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);
     
     if (numeroDeUsuario === numeroSecreto) {
-        asignarTextoElemento('p',`Acertaste el número en ${intentos} ${(intentos === 1) ? 'vez' : 'veces'}`);
-        document.getElementById('reiniciar').removeAttribute('disabled');
+        window.location.href = 'felicitaciones.html';
     } else {
-        //El usuario no acertó.
+        // El usuario no acertó.
         if (numeroDeUsuario > numeroSecreto) {
-            asignarTextoElemento('p','El número secreto es menor');
+            asignarTextoElemento('p', 'El número secreto es menor');
         } else {
-            asignarTextoElemento('p','El número secreto es mayor');
+            asignarTextoElemento('p', 'El número secreto es mayor');
         }
         intentos++;
         limpiarCaja();
@@ -37,7 +35,7 @@ function generarNumeroSecreto() {
 }
 
 function condicionesIniciales() {
-    asignarTextoElemento('h1','Juego del número secreto!');
+    asignarTextoElemento('h1','¡Juego del número secreto¡');
     asignarTextoElemento('p',`Indica un número del 1 al 10`);
     numeroSecreto = generarNumeroSecreto();
     intentos = 1;
